4aa7943bbe7f01724aedacd4be18b82d0b7dfe20
Minwei Xu <faceswilliam@gmail.com>
2017-03-15 17:18:42 -0400

    port

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_restful import Resource, Api
 from scraper import getUrl
-
+import os
 app = Flask(__name__)
 api = Api(app)
 
@@ -12,4 +12,5 @@ class HelloWorld(Resource):
 api.add_resource(HelloWorld, '/<string:hall_id>')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
